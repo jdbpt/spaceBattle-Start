3f156f8d1c54098136e06ee7b37cadd5947954c3
jdbpt <jbrown5.filn@gmail.com>
2023-07-06 15:02:14 -0400

    updated game

--- index.js
+++ index.js
@@ -5,20 +5,18 @@ class Ship{
         this.accuracy = accuracy;
     }
 
+    //if Math.random() less than this.accuracy, then target takes damage equal
+    //to this objects firepower
     fire(target){
         if(Math.random() < this.accuracy){
-            target.takeDamage();
+            target.takeDamage(this.firepower);
         }
 
     }
 
-    takeDamage(name){
-        if(this.shiphull >= 0){
-            this.shiphull -= this.firepower;
-        console.log(name + " took " + this.firepower + " of damage!");
-        } else{
-            console.log(name + " has been destroyed!");
-        }    
+    //takeDamange equal to the firepower going against
+    takeDamage(firepower){
+            this.shiphull -= firepower;
 
     }
 
@@ -37,33 +35,22 @@ class Aliens{
 
     addAliens(){
         let numAliens = 6;
-        
 
         for(let i = 0; i < numAliens; i++){
             let shiphull = Math.floor(Math.random()*(7-3))+3;
             let firepower= Math.floor(Math.random()*(5-2))+2;
-            let accuracy = (Math.random()*(.8-.6))+.6;
+            let accuracy = (Math.random()*(.8-.6)+.6).toFixed(1);
             let newAlienShip = new Ship(shiphull, firepower, accuracy);
             this.ships.push(newAlienShip);
 
         }
-    }
+    }//add Aliens
 }//end Aliens class
 
 //if extending alien based on ship options
 //super(Math.floor(Math.random()*(7-3))+3, Math.floor(Math.random()*(5-2))+2, (Math.random()*(.8-.6))+.6);
-//(Math.floor(Math.random()*(9-6))+1)/10
+//(Math.floor(Math.random()*(8.1-6))+1)/10
 //(Math.random()*(.8-.6))+.6
-
-class Battle{
-    //handles the battles
-}//end Battle class
-
-class Game{
-    //handles the game, calls the Battle
-
-}//end Game class
-
 
 //Understanding Math.random() lecture review
 //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
@@ -74,10 +61,107 @@ class Game{
 // let randomNum1 = Math.floor(Math.random()*10)+1;
 // console.log(randomNum1);
 
+//ship images
+{/* <img src="imgs/enemy_ship.png">
+<img src="imgs/enemy_ship_dead.png">
+<img src="imgs/USS_Ship.png"> */}
+
+
 const maverick = new Ship(20, 5, .7);
 console.log(maverick);
+console.log("********************************");
 
 const alienShips = new Aliens();
 alienShips.addAliens();
 console.log(alienShips);
+console.log("***********************************");
+console.log("SPACE BATTLE TO SAVE THE UNIVERSE!!!!")
 
+//console.log(alienShips.ships[0].accuracy);
+const startButton = document.getElementById("start-game");
+
+const message = document.getElementById("message");
+
+const enemyIntact = document.getElementById("untouched");
+const enemyDestroyed = document.getElementById("destroyed");
+const ship = document.getElementById("ship");
+
+const attackAliens = () => {
+    let enemyFleet = alienShips.ships;
+    let loseGameMessage = "The Maverick hath been Defeated!  The Universe is lost- Game Over";
+    let winGameMessage = "The Maverick has been victorious!  They have saved the Universe from the enemy alien scourge!!!!!";
+    console.log(enemyFleet);
+   
+    for(let i = 0; i < enemyFleet.length; i++){
+        //alert(`The health of the Maverick Shiphull is: ${maverick.shiphull}
+        //They are fighting Enemy Alien Battleship ${i+1}!`);
+        message.innerHTML = `The health of the Maverick Shiphull is: ${maverick.shiphull}
+        //They are fighting Enemy Alien Battleship ${i+1}!`;
+        //check if ship is destoryed, if not keep fighting, if so, end game
+        if(maverick.shiphull<=0){
+            console.log(loseGameMessage);
+            //alert(loseGameMessage);
+            message.innerHTML = loseGameMessage;
+            ship.style.width = "50%";
+            loseGameMessage = "";
+            break;
+
+        } 
+
+        while(maverick.shiphull > 0 && enemyFleet[i].shiphull > 0){
+            console.log(`The current health of the Maverick's shiphull is ${maverick.shiphull}`);
+            maverick.fire(enemyFleet[i]);
+            enemyFleet[i].fire(maverick);
+            console.log("_____________________________");
+        
+            if(enemyFleet[i].shiphull <= 0){
+                console.log("Alien Ship " + (i+1) + " has been destroyed!!!!");
+                //alert("Alien Ship " + (i+1) + " has been destroyed!!!!");
+                //message.innerHTML = "Alien Ship " + (i+1) + " has been destroyed!!!!";
+                break;
+            }
+        }//end while loop
+
+
+    }//end for loop
+
+    //congradulations output if maverick.shiphull is greater than zero
+    if(maverick.shiphull > 0){
+        console.log(winGameMessage);
+        message.innerHTML = winGameMessage;
+        enemyIntact.style.display = "none";
+        enemyDestroyed.style.display = "block";
+        
+    } else{
+        //else if maverick.shipfull < 0 and the loseGameMessage is not empty (if empty this message was shown via the for loop)
+        //give lose message
+        if(loseGameMessage != ""){
+            console.log(loseGameMessage);
+            message.innerHTML = loseGameMessage;
+            ship.style.transform = "width 0.25s"
+            ship.style.width = "50%";
+
+
+            //alert(loseGameMessage);
+        }
+    }//if else
+
+}//attackAliens
+
+let count = 0;
+startButton.addEventListener("click", (event) => {
+    console.log(event.target);
+    enemyIntact.style.display = "block";
+    enemyDestroyed.style.display = "none";
+    ship.style.width = "100%";
+    attackAliens();
+    maverick.shiphull = 20;
+    alienShips.ships = [];
+    alienShips.addAliens();
+
+   
+
+});
+
+//attackAliens();
+console.log(maverick);
